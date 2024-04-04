e3d8c9dc9f272a9c5deadfe5051aeef296a46add
Black-Emblem <kastanias_g@outlook.com>
2024-04-04 14:18:27 +0300

    Initial

--- includes/database.php
+++ includes/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost:3307";
 $username = "root";
 $password = "";
-$dbname = "theaterdb";
+$dbname = "cinemadb";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn)
 {
